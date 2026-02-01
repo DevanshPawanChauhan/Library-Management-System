bf291609db768709879e269f018e67403fbe5f6f
DevanshPawanChauhan <chauhandevansh7203@gmail.com>
2026-02-01 16:19:31 +0530

    Add Member model with borrowed books tracking

--- librarymanagement.py
+++ librarymanagement.py
@@ -24,3 +24,24 @@ class Book:
             data["available_copies"],
             data["total_copies"]
         )
+
+class Member:
+    def __init__(self, member_id, name, borrowed_books):
+        self.member_id = member_id
+        self.name = name
+        self.borrowed_books = borrowed_books  
+
+    def to_dict(self):
+        return {
+            "id": self.member_id,
+            "name": self.name,
+            "borrowed_books": self.borrowed_books
+        }
+
+    @staticmethod
+    def from_dict(data):
+        return Member(
+            data["id"],
+            data["name"],
+            data["borrowed_books"]
+        )
